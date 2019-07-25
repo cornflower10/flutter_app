a72bb58e82bf0ad60e35cd110fa2e088b0c694c2
xiejingbao <xiejb@qlzqzg.com>
2019-07-25 19:18:44 +0800

    update baiduSdk

---
+++ android/app/src/main/kotlin/com/cornflower/flutterapp/App.java
@@ -0,0 +1,22 @@
+package com.cornflower.flutterapp;
+
+import com.baidu.mapapi.CoordType;
+import com.baidu.mapapi.SDKInitializer;
+
+import io.flutter.app.FlutterApplication;
+
+/**
+ * Created by xiejingbao on 2019/7/25.
+ */
+public class App extends FlutterApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        SDKInitializer.initialize(this);
+        //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
+        //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
+        SDKInitializer.setCoordType(CoordType.BD09LL);
+    }
+}
--- android/app/src/main/kotlin/com/cornflower/flutterapp/SecondActivity.kt
+++ android/app/src/main/kotlin/com/cornflower/flutterapp/SecondActivity.kt
@@ -3,13 +3,48 @@ package com.cornflower.flutterapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.baidu.location.BDAbstractLocationListener
+import com.baidu.location.BDLocation
+import com.baidu.location.LocationClient
 import kotlinx.android.synthetic.main.activity_second.*
 
-class SecondActivity : AppCompatActivity() {
 
+class SecondActivity : AppCompatActivity() {
+    lateinit var location :LocationClient
+    var  address:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        tv.text = this.intent.getStringExtra("main")
+        bmapView.map.isMyLocationEnabled = true
+        location = LocationClient(this)
+
+        location.registerLocationListener(MyLocationListener())
+        location.start()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        bmapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        bmapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        location.stop()
+        bmapView.map.isMyLocationEnabled = false
+        bmapView.onDestroy()
+    }
+
+   inner class MyLocationListener : BDAbstractLocationListener() {
+        override fun onReceiveLocation(p0: BDLocation?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            this@SecondActivity.address = p0?.address?.address!!
+
+        }
+
     }
 }
